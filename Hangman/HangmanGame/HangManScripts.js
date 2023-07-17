fb97fe8b7aebc3089682df182c20a44746e8b645
DaligAnka <sony261298@gmail.com>
2023-07-17 21:19:24 +0300

    'FinalCut'

--- Hangman/HangmanGame/HangManScripts.js
+++ Hangman/HangmanGame/HangManScripts.js
@@ -115,19 +115,24 @@ for(let i=65;i<91;i++){
 button.innerText = String.fromCharCode(i);
 button.addEventListener("click", () => {
     let charArray = chosenWord.split("");
-    let dashes = document.getElementsByClassName("dashes"); 
+    let dashes = document.getElementsByClassName("dashes")[0]; 
     if (charArray.includes(button.innerText)){
         charArray.forEach((char, index) => {
         
             if(char === button.innerText) {
                 //  ТУТ БЛЯТЬ КАКАЯ ТО ОШИБКА
               
-                dashes[index].innerText = char;
+
+                const strArray = Array.from(dashes.innerText);
+                strArray[index] = char;
+                dashes.innerText = strArray.join("");
+
+        //         dashes[index].innerText = char;
                 winCount += 1;
-        console.log(dashes)
-                if(winCount == charArray.length) {
+        // console.log(dashes)
+                if(winCount == strArray.length) {
                     resultText.innerHTML= `<h2 class ='win-msg'> You win! You have rescued him from....fins!</h2>
-                    <p>The word was<span> ${chosenWord}</span></p>`;
+                    <p>The word was <span> ${chosenWord}</span></p>`;
                     blocker();
                 
                     
@@ -136,8 +141,18 @@ button.addEventListener("click", () => {
 
         });
     } 
-    
-
+    else {
+        count +=1;
+        drawFisherMan(count);
+        //count 6 потому что 1 голова. 1 тело. 2 руки. 2 ноги.
+        console.log(count);
+        if (count == 6) {
+            resultText.innerHTML =`<h2 class ='lose-msg'> Oh no! You lose. The fisherman was hanged by the fish...</h2>
+            <p>The last word was <span> ${chosenWord}</span></p>`;
+            blocker()
+        }
+    }
+button.disabled = true;
 });
 
 letterContainer.appendChild(button);
@@ -146,7 +161,99 @@ letterContainer.appendChild(button);
 
 
 displayOptions(); 
+
+let {intitialDrawing} = canvasCreator ();
+intitialDrawing();
+
 };
+
+
+
+
+
+const canvasCreator = () => {
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.strokeStyle = "#000";
+    context.lineWidth = 2;
+
+
+    const drawLine =(fromX, fromY, toX, toY) => {
+        context.moveTo(fromX, fromY);
+        context.lineTo(toX, toY);
+        context.stroke();
+    };
+
+
+    const head = () => {
+        context.beginPath();
+        context.arc(70, 30, 10, 0, Math.PI*2,true);
+        context.stroke();
+};
+
+
+const body = () => {
+    drawLine(70,40,70,80);
+};
+
+const leftArm = () => {
+    drawLine(70,50,50,70)
+};
+const rightArm = () => {
+    drawLine(70,50,90,70)
+};
+const leftLeg = () => {
+    drawLine(70,80,50,110);
+};
+const rightLeg = () => {
+    drawLine(70, 80, 90, 110);
+};
+
+const intitialDrawing = () => {
+    context.clearRect(0,0, context.canvas.width, context.canvas.height);
+    drawLine(10, 130, 130, 130); //АБ инитио нижняя линия
+    drawLine(10, 10, 10,131); // левая линия
+    drawLine(10,10,70,10); // верззняя линия
+    drawLine(70, 10, 70, 20) // край виселицы верхний
+};
+
+return{intitialDrawing,head,body,leftArm,rightArm,leftLeg,rightLeg};
+};
+
+
+const drawFisherMan = (count) => {
+    let {head,body,leftArm,rightArm,leftLeg,rightLeg} = canvasCreator ();
+    switch (count) {
+        case 1:
+            head();
+            break;
+
+            case 2:
+                body();
+                break;
+
+                case 3:
+                    leftArm();
+                    break;
+
+                    case 4:
+                    rightArm();
+                    break;
+
+                    case 5:
+                    leftLeg();
+                    break;
+
+                    case 6:
+                        rightLeg();
+
+                        default: break;
+                       
+    }
+};
+
+
+
 
 
 newGameButton.addEventListener("click", initializer);
